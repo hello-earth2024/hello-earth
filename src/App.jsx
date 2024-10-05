74c967490e0eafd0e536fd7c73fcd32635cb534a
Allan <allanjungman8@gmail.com>
2024-10-05 16:15:35 -0300

    Added quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Quiz from 'react-quiz-component';
+import { quiz } from './assets/quiz';
+
 function App() {
   return (
     <>
       Hello, Earth! We're going on!
+      <Quiz quiz={quiz} shuffle={true} />
     </>
   )
 }
