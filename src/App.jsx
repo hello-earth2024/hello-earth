0c5ee390721f613e169707d430395a7427f4538f
Lucas Cardoso <lcardosopereira05@gmail.com>
2024-10-05 16:45:44 -0300

    Started landing page and installed MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Quiz from 'react-quiz-component';
 import { quiz } from './assets/quiz';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <>
-      Hello, Earth! We're going on!
-      <Quiz quiz={quiz} shuffle={true} />
+      {/* Hello, Earth! We're going on!
+      <Quiz quiz={quiz} shuffle={true} /> */}
+
+      <LandingPage />
     </>
   )
 }
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import '../assets/css/landing_page.css';
+
+export default function LandingPage() {
+    return <>
+        <main>
+            <section className="welcome">
+                <h1>Hello, Earth!</h1>
+                <p>It's time to navigate into flying rivers to discover how Earth is connected to itself and to people</p>
+            </section>
+        </main>
+    </>
+}
