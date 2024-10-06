b7ca419ab29bd0cb101571aef670ae3b6ad1c332
Lucas Cardoso <lcardosopereira05@gmail.com>
2024-10-06 10:58:53 -0300

    Added typewriter effect on text and improved footer. Also added landing page's texts

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import { Button } from '@mui/material';
+import { Typewriter } from 'react-simple-typewriter';
 import '../assets/css/landing_page.css';
 import ScrollToTop from "react-scroll-to-top";
 import nasaLogo from '../assets/img/spaceappslogo.png'
@@ -15,7 +16,20 @@ function App() {
         <>
             <div className="background">
                 <div className="content">
-                    <h1 className="title">Hello, Earth!</h1>
+                    <h1 className="title">
+                        <Typewriter
+                            cursor
+                            cursorBlinking
+                            cursorColor="#ffffff"
+                            delaySpeed={1000}
+                            deleteSpeed={50}
+                            loop={0}
+                            typeSpeed={100}
+                            words={[
+                                'Hello, Earth!',
+                            ]}
+                        />
+                    </h1>
                     <p className="subtitle">
                         It’s time to navigate into flying rivers to discover how Earth is connected to <span className="highlight">itself and to people.</span>
                     </p>
@@ -28,8 +42,7 @@ function App() {
                 <div className="text-content">
                     <h2 className="section-title">Our Earth is fully connected.</h2>
                     <p className="section-paragraph">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        <span className="highlight"> Flying rivers </span> are an example of this interconnection, and here you’ll know them better.
+                        Imagine Earth as a <span className="highlight">big puzzle</span>, where each piece is important so that everything works in harmony. In this puzzle, the air we breathe, the forests, the ocean, and even the little soil worms are <span className="highlight">connected</span>. Each piece (or system) of our planet influences and is influenced by the others, building what we call a <span className="highlight">system of systems</span>. In this reasoning, think about the climate crisis we live as <span className="highlight">changes</span> on these systems, where a little, single change in one of them can affect all others and, together, cause <span className="highlight">global catastrophes</span>.
                     </p>
                 </div>
                 <div className="image-content">
@@ -43,24 +56,27 @@ function App() {
                 <div className="text-content">
                     <h2 className="section-title">Flying rivers: connecting the Earth.<br></br>And its people.</h2>
                     <p className="section-paragraph">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        <span className="highlight"> Flying rivers </span> are an example of this interconnection, and here you’ll know them better.
+                        Water is constantly on the move, flowing from rivers to oceans, shaping how societies function along the way. But water doesn't just travel on land — it also moves through the sky, offering us a new perspective on how <span className="highlight">climate change is affecting everything</span>. This brings us to the concept of <span className="highlight">Flying Rivers</span>. These are powerful streams of water vapor that form above rainforests like the Amazon and travel across large, continental distances, bringing moisture to regions <span className="highlight">far from their source</span>. These invisible rivers play a critical role in maintaining rainfall patterns, supporting agriculture, and sustaining biodiversity across continents. As deforestation and climate change continue to disrupt these natural systems, it's more important than ever <span className="highlight">to understand and protect flying rivers</span> for the sake of ecosystems and the people who rely on them.
+
                     </p>
                 </div>
             </div>
             <footer>
-                <div className="left">
-                    <p>Resources</p>
-                    <ul>
-                        <li>Our GitHub repo</li>
-                        <li>NASA Space Apps' Project Page</li>
-                        <li>About us</li>
-                    </ul>
+                <div className="wrapper">
+                    <div className="left">
+                        <p>Resources</p>
+                        <ul>
+                            <li>Our GitHub repo</li>
+                            <li>NASA Space Apps' Project Page</li>
+                            <li>About us</li>
+                        </ul>
+                    </div>
+                    <div className="right">
+                        <img src={nasaLogo} alt="" />
+                        <p>This project is part of NASA International Space Apps Challenge '24.</p>
+                    </div>
                 </div>
-                <div className="right">
-                    <img src={nasaLogo} alt="" />
-                    <p>This project is part of NASA International Space Apps Challenge '24.</p>
-                </div>
+                <p>Made with ❤️ from 🇧🇷 to the 🌎<br></br>&copy; Hello, Earth! Team, 2024.</p>
             </footer>
             <ScrollToTop smooth />
         </>
